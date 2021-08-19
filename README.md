65b7154111c2dc2a193f58b1aebef0fff835f351
PiciAkk <marci@dbx.hu>
2021-08-19 14:07:52 +0200

    big update... currently only works on zsh... bash compatibility coming soon...

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # kotlinc-native-install
 Easy-to-use installer for Kotlin/Native command-line compiler
+*Note: Linux-only*
 
 ## Installation guide:
 ---
 ### Manual:
-...
+1.) Clone this GitHub repository using: git clone https://github.com/piciakk/kotlinc-native-install
+2.) Go to the GitHub repo, you just cloned: cd kotlinc-native-install
+3.) Install kotlinc-native: chmod +x ./install.sh && ./install.sh
 ### Automatic:
-...
+wget -qO- blahblah
--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 # download
-curl -s https://api.github.com/repos/JetBrains/Kotlin/releases/latest \
-| grep "browser_download_url.*linux*.*tar.gz" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
-tar -xzf kotlin-native-linux
-mv ./kotlin-native-linux ~/
+downloadFunction(){
+  curl -s https://api.github.com/repos/JetBrains/Kotlin/releases/latest \
+  | grep "browser_download_url.*linux*.*tar.gz" \
+  | cut -d : -f 2,3 \
+  | tr -d \"
+}
+downloadURL=$(downloadFunction)
+export fileName=$(basename "$downloadURL")
+wget -q $downloadURL
+# extract
+tar -xzf $fileName
+# move
+export folderName=$(echo $fileName | sed 's/.tar.gz//g')
+mv ./$folderName ~/
 # add to path
-if [$shell == "zsh"]
+if [$(awk -F: -v u="$USER" 'u==$1&&$0=$NF' /etc/passwd) =- "zsh"]
 then
-  path+=('~/kotlin-native-linux/bin')
+	echo "path+=~/$folderName/bin" >> ~/.zshrc
+elif [$(awk -F: -v u="$USER" 'u==$1&&$0=$NF' /etc/passwd) =- "bash"]
+then
+  echo "You are using bash..."
 else
-  PATH='~/kotlin-native-linux/bin':$PATH
-if
+  echo "You are using an unsupported shell..."
+fi
